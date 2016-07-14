b13b0a322a440f653eb2ee9d1c2be50fc5ba3683
yscoder <634206017@qq.com>
2016-07-14 16:55:31 +0800

    fix 当json-content未配置时搜索会出错, by #55

--- source/js/search.js
+++ source/js/search.js
@@ -17,7 +17,8 @@
 
             xhr.onload = function() {
                 if (this.status >= 200 && this.status < 300) {
-                    searchData = JSON.parse(this.response);
+                    var res = JSON.parse(this.response);
+                    searchData = res instanceof Array ? res : res.posts;
                     success(searchData);
                 } else {
                     console.error(this.statusText);

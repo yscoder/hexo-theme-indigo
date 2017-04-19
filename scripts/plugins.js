9f6c52b474b88797b7b8348232de789a831d6518
yscoder <634206017@qq.com>
2017-04-19 22:01:20 +0800

    Use the latest version of cdn

--- scripts/plugins.js
+++ scripts/plugins.js
@@ -5,7 +5,7 @@ hexo.extend.helper.register('theme_version', () => version)
 const source = (path, cache, ext) => {
     if (cache) {
         const minFile = `${path}${ext === '.js' ? '.min' : ''}${ext}`
-        return hexo.theme.config.cdn ? `//unpkg.com/${name}@${version}${minFile}` : `${minFile}?v=${version}`
+        return hexo.theme.config.cdn ? `//unpkg.com/${name}@latest${minFile}` : `${minFile}?v=${version}`
     } else {
         return path + ext
     }
